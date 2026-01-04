e16b82b9aec32ddd30306ec7087658635023b1a2
sibirturan <128335706+sibirturan@users.noreply.github.com>
2026-01-03 20:24:27 -0500

    Create firebase.ts

---
+++ src/services/firebase.ts
@@ -0,0 +1,4 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'; // ← BUNU EKLE
